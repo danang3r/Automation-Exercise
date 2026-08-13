ead69432697c170ecf95c0529c88918342b28fec
danang3r <mssdmchls@gmail.com>
2026-08-13 19:31:50 +0300

    code push via ssh

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
 test('example test', async ({ page }) => {
-  await page.goto('https://google.com');
+  await page.goto('https://google.com/');
   // Додайте свої тести тут
 });
