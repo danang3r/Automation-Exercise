f81338406eec80146c07a6bd388d98aebb10cdd3
danang3r <mssdmchls@gmail.com>
2026-08-22 19:18:38 +0300

    commented row removed

--- tests/API/postToAllProductsList.spec.ts
+++ tests/API/postToAllProductsList.spec.ts
@@ -5,7 +5,6 @@ test('Verify that user cannot POST to all products list', async ({ request }) =>
     data: {id: 1, name: 'Product 1', price: '10.00', brand: 'Brand A', category: {category: 'Category 1', usertype: {usertype: 'User Type 1'}}}
   });
 
-  //expect(response.status()).toBe(200);
   expect(await response.json()).toMatchObject({
     responseCode: 405,
     message: 'This request method is not supported.',
